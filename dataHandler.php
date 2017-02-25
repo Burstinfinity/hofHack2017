e55dda9641f794f344afcd3a6170ef1777259daa
Amy Topka <atopka1@gmail.com>
2017-02-25 00:09:45 -0500

    Added Data Handler

--- dataHandler.php
+++ dataHandler.php
@@ -16,9 +16,39 @@
     return $result;
   }
 
-  //takes json from post content and returns json from db
-  function getProduct(post_score) {
+  function checkRange($emotion, $value, $range) {
+    $sql = "SELECT * FROM ProductTable WHERE ".$emotion." BETWEEN ".($value - $range)." AND ".($value + $range);
+    $result = executeQuery($sql);
+    if ($result->num_rows == 0) {
+      return [];
+    }
+    else {
+      $products = array();
+      while ($row = $result->fetch_assoc()) {
+        $product = array(
+          "product_id"=>$row["product_id"],
+          "name"=>$row["name"],
+          "image"=>$row["image"],
+          "anger"=>$row["anger"],
+          "joy"=>$row["joy"],
+          "sadness"=>$ro
+        );
+      }
+    }
+  }
 
+  //takes json from post content and returns json from db
+  function getProduct($post_score) {
+    //select with each emotion in range
+    $anger = $post_score["anger"];
+    $fear = $post_score["fear"];
+    $sadness = $post_score["sadness"];
+    $disgust = $post_score["disgust"];
+    $joy = $post_score["joy"];
+
+    $range = 0.2;
+
+    $sql = "SELECT * FROM ProductTable WHERE anger BETWEEN ".$anger
   }
 
 ?>
