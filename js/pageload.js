52e94abc84c2a73615c00666a46dd76e1f6ad8d6
john <johnxue88@yahoo.com>
2017-02-25 04:51:27 -0500

    more merge conlcits

--- js/pageload.js
+++ js/pageload.js
@@ -2,11 +2,8 @@ $(document).ready(function() {
 
   if (window.location.hash) {
     href = location.hash.slice(1) + ".html";
-<<<<<<< HEAD
-    $("#div-main").load(href)l
-=======
+
     $("#div-main").load(href);
->>>>>>> origin/Cheryl
   }
   else {
     window.location.hash = "#login";
@@ -16,9 +13,5 @@ $(document).ready(function() {
     var href = location.hash.slice(1) + ".html";
     $("#div-main").load(href);
   });
-<<<<<<< HEAD
-  
-=======
 
->>>>>>> origin/Cheryl
 })
